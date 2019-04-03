35c2390a9fb0580ebbc5e4733bb528f036806283
LQFGH <15809601162@163.com>
2019-04-03 12:59:34 +0800

    init project

---
+++ src/main/java/com/feng/jenkins/controller/JenkinsController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.feng.jenkins.controller;
+
+/**
+ * @author Lee
+ *
+ */
+public class JenkinsController {
+
+	public String jenkins() {
+		return "Hello Jenkins";
+	}
+	
+}
